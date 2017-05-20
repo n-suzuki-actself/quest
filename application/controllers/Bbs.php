fde7892f495147312fca5f26b24a58efc6a85922
nozomi <pontanet31@gmail.com>
2017-05-20 20:02:04 +0900

    初期設定

---
+++ application/controllers/Bbs.php
@@ -0,0 +1,8 @@
+<?php
+class Bbs extends CI_Controller {
+
+    public function index()
+    {
+        $this->load->view('menu');
+    }
+}
---
+++ application/views/menu.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <title>Test Page</title>
+    </head>
+    <body>
+        <?php echo date('Y-m-d'); ?>
+    </body>
+</html>
